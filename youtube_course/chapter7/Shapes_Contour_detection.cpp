a41ab9487f6eb5b33203c5e51792693399af6074
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-01-31 17:43:45 -0800

    Update some files.

---
+++ youtube_course/chapter7/Shapes_Contour_detection.cpp
@@ -0,0 +1,36 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
+
+int main()
+{
+    string path ="Resources/shapes.png";
+    Mat img = imread(path);
+
+    cvtColor(img, imgGray, COLOR_BGR2GRAY);
+    GaussianBlur(imgGray, imgBlur, Size(3,3), 3, 0);
+    Canny(imgBlur, imgCanny, 25, 75);
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(3,3));
+    dilate(imgCanny, imgDil, kernel);
+
+
+    imshow("Image",img);
+     imshow("Image Gray",imgGray);
+      imshow("Image blur",imgBlur);
+       imshow("Image Canny",imgCanny);
+        imshow("Image Dil",imgDil);
+         imwrite("Results/Image.png",img);
+     imwrite("Results/Image Gray.png",imgGray);
+      imwrite("Results/Image blur.png",imgBlur);
+       imwrite("Results/Image Canny.png",imgCanny);
+        imwrite("Results/Image Dil.png",imgDil);
+        // imshow("Image Erode",imgErode);
+
+}
+//export DISPLAY=:0; g++ -o tst1 Shapes_Contour_detection.cpp `pkg-config opencv4 --cflags --libs`
